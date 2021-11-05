427c6c3b6190f3950bc62c1589b328592e4c36fc
mlee-cloud <darum.mlee@gmail.com>
2021-11-05 09:34:27 +0900

    detail page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="detail">
     <h1>This is movie detail page</h1>
-    <div class="detail_visual" :style="{backgroundImage:`url(http://image.tmdb.org/t/p/w1920_and_h800_multi_faces${info.backdrop_path})`}">
+    <div class="detail_visual" :style="bg ? {backgroundImage:`url(http://image.tmdb.org/t/p/w1920_and_h800_multi_faces${bg})`}:''">
       <div class="dim"></div>
       <div class="container">
         <div class="detail_info">
           <div class="movie_img">
-            <img :src="`http://image.tmdb.org/t/p/w300${info.poster_path}`" alt="">
+            <img :src="poster ? `http://image.tmdb.org/t/p/w300${poster}` : ''" alt="">
           </div>
           <div class="cont">
-            <h1>{{info.title}}</h1>
-            <span class="release_date">({{releaseDate}})</span>
+            <h1>{{title}}</h1>
+            <span class="release_date">({{releaseYear}})</span>
+            <div class="facts">
+              <span class="release">{{releaseDate}}</span>
+              <i>·</i>
+              <span class="genres">{{genres}}</span>
+              <i>·</i>
+              <span class="runtime">{{runtime}}</span>분
+            </div>
+            <ul class="movie_actions">
+              <li>
+                <div class="chart">
+                  <div>
+                    <span>
+                      <strong>{{voteAverage}}</strong>%
+                    </span>
+                  </div>
+                  <canvas id="myChart" ></canvas>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -30,16 +49,37 @@ export default {
   data(){
     return {
       info: null,
-      imgSrc: null,
-      releaseDate: null
+      bg: null,
+      poster:null,
+      title: null,
+      imgSrc: 'http://image.tmdb.org/t/p/w400/rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg',
+      releaseYear: null,
+      releaseDate: null,
+      runtime:null,
+      genres:null,
+      voteAverage:0
     }
   },
   created(){
     console.log("query=",this.$route.params.movieID)
+    
     api.moviesDetail(this.$route.params.movieID).then( res => {
-      console.log("res=", res.data);
+      
       this.info = res.data; 
-      this.releaseDate = res.data.release_date.substring(0,4);
+      this.bg = res.data.backdrop_path;
+      this.poster = res.data.poster_path;
+      this.title = res.data.title;
+      this.releaseYear = res.data.release_date.substring(0,4);
+      this.releaseDate = res.data.release_date;
+      this.runtime = res.data.runtime;
+      this.genres = res.data.genres[0].name;
+      this.voteAverage = res.data.vote_average * 10;
+      if(res.data.backdrop_path) {
+        console.log("backdrop_path=", res.data.backdrop_path);
+      }
+      else{
+        console.log("backdrop_path=", "空值");
+      }
       //이미지 주요색상
       this.imgSrc = `http://image.tmdb.org/t/p/w400${res.data.backdrop_path}`;
       let result = rgbaster(
@@ -50,14 +90,49 @@ export default {
       )
       result.then((res)=>{
         console.log(res[0].color);
-        // document.getElementsByTagName('body')[0].style.background = res[0].color;
+        //document.getElementsByTagName('body')[0].style.background = res[0].color;
         document.querySelector('.dim').style.backgroundColor = res[0].color;
       })
       console.log("imgSrc=", this.imgSrc);
+
+      //chart 
+      var ctx = document.getElementById('myChart').getContext('2d');
+      
+
+
+      var data = {
+        datasets: [{
+        //label: 'My First Dataset',
+        labels: ['Red', 'Blue'],
+        data: [this.voteAverage,(100-this.voteAverage)],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)'
+        ],
+        hoverOffset: 0,
+        spacing: 0,
+        borderWidth: 0,
+        cutout: "70%",
+        //radius: "100%",
+        animation: false,
+        //circumference: 180
+        borderAlign: 'inner'
+        
+        }]
+      };
+
+      // For a pie chart
+      var myBarChart = new Chart(ctx, {
+        type: '',
+        data: data
+      });
+      
     }).catch(err => {
       console.log("error", err);
-    })
+    });
 
+    
+    
     
   },
   mounted(){
